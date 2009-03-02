97821899235f73069d3bcce9c65acbbfe1116bc6
Noah Paessel <knowuh@gmail.com>
2009-03-02 09:29:19 -0500

    small file cleanup

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -1,4 +1,5 @@
 class Bundle < ActiveResource::Base
+  # TODO: dont hardcode he server address!
   self.site = "http://localhost:3000/"
   self.prefix='/13/'
 end
--- app/models/workgroup.rb
+++ app/models/workgroup.rb
@@ -1,4 +1,6 @@
 class Workgroup < ActiveResource::Base
+  # TODO: dont hardcode he server address!
   self.site = "http://localhost:3000/"
+  # TODO: Portal prefix should NOT be hard-coded.
   self.prefix='/13/' #thats loops I think
 end
